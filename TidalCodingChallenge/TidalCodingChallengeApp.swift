d70c6613847089fd7b29dfae5623744376411aab
michael.aidoo <creativemic24@gmail.com>
2022-03-12 13:18:34 +0100

    Initial Commit

---
+++ TidalCodingChallenge/TidalCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TidalCodingChallengeApp.swift
+//  TidalCodingChallenge
+//
+//  Created by Michael Aidoo on 2022-03-12.
+//
+
+import SwiftUI
+
+@main
+struct TidalCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
